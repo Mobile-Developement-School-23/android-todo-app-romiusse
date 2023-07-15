08f4f1c8f6913c00d118c0a5e75fec1dac8d8c99
Romiusse <me.event2015@yandex.ru>
2023-07-15 07:59:23 +0300

    added bottom sheet

--- feature/edit_todo/src/main/java/com/romiusse/edit_todo/compose/AddScreenCompose.kt
+++ feature/edit_todo/src/main/java/com/romiusse/edit_todo/compose/AddScreenCompose.kt
@@ -5,45 +5,55 @@ package com.romiusse.edit_todo.compose
 import android.annotation.SuppressLint
 import android.app.TimePickerDialog
 import android.content.res.Configuration
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -55,16 +65,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
-import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.timepicker.MaterialTimePicker
 import com.romiusse.edit_todo.R
 import com.romiusse.edit_todo.screen.AddScreenViewModel
 import com.romiusse.utils.Utils
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
 import java.util.Calendar
-import java.util.Date
-import java.util.Locale
 
 
 class AddScreenCompose {
@@ -101,13 +107,28 @@ class AddScreenCompose {
 
         var scrollState = rememberScrollState()
 
+        val scaffoldState = rememberBottomSheetScaffoldState()
+
         var elevation: Dp = min(16.dp, (scrollState.value * 0.3).toInt().dp)
 
+        val priorityItems = stringArrayResource(id = R.array.priority).toList()
+        var selectedText = remember {
+            mutableStateOf(
+                if(viewModel != null)
+                    priorityItems[Utils.getPosFromPriority(viewModel.item.priority)]
+                else priorityItems[0]
+            )
+        }
+        var openBottomSheet = rememberSaveable { mutableStateOf(false) }
+        viewModel?.let { model ->
+            model.posSetChangedListener = {
+                selectedText.value = priorityItems[Utils.getPosFromPriority(viewModel.item.priority)]
+            } }
         Scaffold(topBar = {AppBar(elevation, saveListener, closeListener )}){
             Column(modifier = Modifier.verticalScroll(scrollState)) {
-                Box(modifier = Modifier.padding(top = 64.dp)){}
+                Box(modifier = Modifier.padding(top = 72.dp)){}
                 EditText(viewModel)
-                Priority(viewModel)
+                Priority(openBottomSheet, selectedText)
                 Divider(modifier = Modifier.padding(vertical = 16.dp, horizontal = 16.dp))
                 Deadline(viewModel, switchOnclickListener, calendarShowListener)
                 Divider(modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp))
@@ -115,6 +136,7 @@ class AddScreenCompose {
                 Box(modifier = Modifier.padding(bottom = 264.dp)){}
             }
         }
+        BottomSheet(viewModel, selectedText, priorityItems, openBottomSheet)
 
     }
 
@@ -214,20 +236,130 @@ class AddScreenCompose {
     }
 
     @Composable
-    fun Priority(viewModel: AddScreenViewModel?){
+    fun Priority(
+        openBottomSheet: MutableState<Boolean>,
+        selectedText: MutableState<String>,
+    ){
         Column {
-
+            val scope = rememberCoroutineScope()
             Text(text = stringResource(id = R.string.important),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 8.dp, start = 16.dp) ,
+                    .padding(bottom = 16.dp, start = 16.dp),
                 style = MaterialTheme.typography.bodyLarge)
 
-            PriorityDropDownMenu(viewModel)
+
+            Text(text = selectedText.value,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .clickable {
+                        scope.launch {
+                            openBottomSheet.value = !openBottomSheet.value
+                        }
+                    },
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            //BottomSheet()
+            //PriorityDropDownMenu(viewModel)
 
         }
 
 
+
+    }
+
+    @Composable
+    fun BottomSheet(
+        viewModel: AddScreenViewModel?,
+        selectedText: MutableState<String>,
+        priorityItems: List<String>,
+        openBottomSheet: MutableState<Boolean>
+    ) {
+        val bottomSheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = false
+        )
+
+        // App content
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            // Sheet content
+            if (openBottomSheet.value) {
+                ModalBottomSheet(
+                    onDismissRequest = { openBottomSheet.value = false },
+                    sheetState = bottomSheetState,
+                ) {
+                    Column(Modifier.fillMaxWidth()) {
+                        Button(
+                            onClick = {
+                                selectedText.value = priorityItems[0]
+                                viewModel?.item?.priority = Utils.convertPosToPriority(0)
+                                openBottomSheet.value = false
+
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(text = priorityItems[0])
+                        }
+                        Button(
+                            onClick = {
+                                selectedText.value = priorityItems[1]
+                                viewModel?.item?.priority = Utils.convertPosToPriority(1)
+                                openBottomSheet.value = false
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        ) {
+                            Text(text = priorityItems[1])
+                        }
+
+
+                        var isAnimated by remember { mutableStateOf(false) }
+                        val bgColor = if(isSystemInDarkTheme()) Color.White else Color.Black
+                        val color = remember { Animatable(bgColor) }
+
+                        LaunchedEffect(isAnimated) {
+                            if(isAnimated) {
+                                color.animateTo(
+                                    Color.Red,
+                                    animationSpec = tween(200)
+                                )
+                                color.animateTo(
+                                    bgColor,
+                                    animationSpec = tween(200)
+                                )
+                                delay(100)
+                                isAnimated = false
+                                openBottomSheet.value = false
+                            }
+
+                        }
+
+                        Button(
+                            onClick = {
+                                selectedText.value = priorityItems[2]
+                                viewModel?.item?.priority = Utils.convertPosToPriority(2)
+                                isAnimated = true
+                                      //openBottomSheet.value = false
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            colors = ButtonDefaults.buttonColors(containerColor = color.value)
+                        ) {
+                            Text(text = priorityItems[2])
+                        }
+                    }
+                }
+            }
+        }
 
     }
 
@@ -337,7 +469,7 @@ class AddScreenCompose {
                     Text(
                         text = deadline,
                         modifier = Modifier
-                            .padding(bottom = 8.dp, start = 16.dp)
+                            .padding(bottom = 24.dp, start = 16.dp)
                             .clickable { calendarShowListener.invoke() },
                         style = MaterialTheme.typography.bodyLarge
                     )
@@ -396,6 +528,7 @@ class AddScreenCompose {
 
         }
     }
+
 }
 
 
